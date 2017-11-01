7487f8264f38adc302409838fde51516696b87ff
helin <java_hl@163.com>
2017-11-01 16:20:00 +0800

    添加模板打包后，支持直接通过file:///方式访问

--- template/config/index.js
+++ template/config/index.js
@@ -10,7 +10,8 @@ module.exports = {
     index: path.resolve(__dirname, '../dist/index.html'),
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    // Support the definition of configuration path "./" this way direct access through the file:///
+    assetsPublicPath: './',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
