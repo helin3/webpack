77858e195b9b4cd13944977e3e7f92bf9157e6d4
helin <java_hl@163.com>
2017-11-01 16:18:52 +0800

    模板打包后，支持直接通过file:///方式访问

--- template/build/build.js
+++ template/build/build.js
@@ -11,7 +11,7 @@ const webpack = require('webpack')
 const config = require('../config')
 const webpackConfig = require('./webpack.prod.conf')
 
-const spinner = ora('building for production...')
+const spinner = ora('building for ' + process.env.NODE_ENV + '...')
 spinner.start()
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
@@ -35,7 +35,8 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     console.log(chalk.cyan('  Build complete.\n'))
     console.log(chalk.yellow(
       '  Tip: built files are meant to be served over an HTTP server.\n' +
-      '  Opening index.html over file:// won\'t work.\n'
+      '  Opening index.html over file:// won\'t work.\n' + 
+      '  You can configure `../config/index.js build.assetsPublicPath` as a relative path to fixed.\n'
     ))
   })
 })
