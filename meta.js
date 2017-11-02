9d2f3592f45e58e378239d2408d72ce3549a70e1
helin <java_hl@163.com>
2017-11-02 10:19:00 +0800

    修改模板默认描述

--- meta.js
+++ meta.js
@@ -18,7 +18,7 @@ module.exports = {
       "type": "string",
       "required": false,
       "message": "Project description",
-      "default": "A Vue.js project"
+      "default": "A Frontend Development Platform"
     },
     "author": {
       "type": "string",
